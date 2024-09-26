de2c85fed145c040ef12966c5d22532b78fdc12f
JoeJoeflyn <thaitainguyen336@gmail.com>
2024-09-26 16:10:21 +0700

    feat: adding swagger doku

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Learning Nest')
+    .setDescription('The Learning Nest API description')
+    .setVersion('1.0')
+    .addTag('learning-nest')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 8081);
+}
+bootstrap();
